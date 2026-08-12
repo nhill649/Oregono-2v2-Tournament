43c3048147eb38934ae53aac01829b3ed045aef8
nhill649 <nolanhill6409@gmail.com>
2026-08-11 21:25:35 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,17 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBEuP98gu80mX1J2xKSGqYsVYG8uEk-I",
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEuP98gu00m1X2JxKSGqYsVYG8oU_ek-I",
   authDomain: "oregano-2v2-tournament.firebaseapp.com",
   projectId: "oregano-2v2-tournament",
   storageBucket: "oregano-2v2-tournament.firebasestorage.app",
   messagingSenderId: "921367151742",
   appId: "1:921367151742:web:d658598249ee300feb042f"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
