9098a54074942bd521fef11dffc68fdd294e1daa
nhill649 <nolanhill6409@gmail.com>
2026-08-11 21:02:00 -0400

    Update Firebase configuration with new API key

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBEuP98gu80mX1J2xKSGqYsVYG8uEk-I",
+  authDomain: "oregano-2v2-tournament.firebaseapp.com",
+  projectId: "oregano-2v2-tournament",
+  storageBucket: "oregano-2v2-tournament.firebasestorage.app",
+  messagingSenderId: "921367151742"",
+  appId: "1:921367151742:web:d658598249ee300feb042f"
+};
