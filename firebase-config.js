edc91f119d7f31aaa14e281005c0975ddbcda40c
nhill649 <nolanhill6409@gmail.com>
2026-08-11 21:06:11 -0400

    Fix Firebase web app config syntax

--- firebase-config.js
+++ firebase-config.js
@@ -3,6 +3,6 @@ export const firebaseConfig = {
   authDomain: "oregano-2v2-tournament.firebaseapp.com",
   projectId: "oregano-2v2-tournament",
   storageBucket: "oregano-2v2-tournament.firebasestorage.app",
-  messagingSenderId: "921367151742"",
+  messagingSenderId: "921367151742",
   appId: "1:921367151742:web:d658598249ee300feb042f"
 };
